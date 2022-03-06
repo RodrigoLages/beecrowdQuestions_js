eabd00ae6816f4026633edc56dc53f81b8a03be0
RodrigoLages <rodrigo_lagesbarbosa@hotmail.com>
2022-03-06 11:47:46 -0300

    Top Area

---
+++ 1188.js
@@ -0,0 +1,26 @@
+//var prompt = function(texto) { return lines.shift();};
+
+var operation = prompt();
+var M = [];
+var sum = 0;
+
+for (let i = 0; i < 12; i++) {
+  var aux = [];
+  for (let n = 0; n < 12; n++) {
+    aux.push(parseFloat(prompt()));
+  }
+  M.push(aux);
+}
+
+for (let i = 7; i < 12; i++) {
+  for (let n = 12 - i; n < i; n++) {
+    sum = sum + M[i][n];
+  }
+}
+
+if (operation == "M") {
+  var avg = sum / 30;
+  console.log(avg.toFixed(1));
+} else {
+  console.log(sum.toFixed(1));
+}
