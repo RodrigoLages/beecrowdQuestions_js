345f5d24ab71dbc47c7b6e40201d21945eb2f811
RodrigoLages <rodrigo_lagesbarbosa@hotmail.com>
2022-03-07 13:20:15 -0300

    Ho Ho Ho

---
+++ 1759.js
@@ -0,0 +1,9 @@
+//var prompt = function(texto) { return lines.shift();};
+
+var ammount = parseInt(prompt());
+var ho = "Ho";
+
+for (let i = 1; i < ammount; i++) {
+  ho = ho + " Ho";
+}
+console.log(ho + "!");
