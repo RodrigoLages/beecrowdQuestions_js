c765c1cd6b689c394b58d5b65106326c39795412
RodrigoLages <rodrigo_lagesbarbosa@hotmail.com>
2022-03-07 11:43:46 -0300

    Square Matrix III

---
+++ 1557.js
@@ -0,0 +1,23 @@
+//var prompt = function(texto) { return lines.shift();};
+
+var size = parseInt(prompt());
+
+while (size > 0) {
+  var T = Math.pow(4, size - 1);
+  T = T.toFixed(0).length;
+
+  for (let L = 0; L < size; L++) {
+    let line = [];
+
+    for (let C = 0; C < size; C++) {
+      let aux = Math.pow(2, L + C).toFixed(0);
+
+      line.push(aux.padStart(T));
+    }
+
+    console.log(line.join(" "));
+  }
+
+  console.log("");
+  var size = parseInt(prompt());
+}
