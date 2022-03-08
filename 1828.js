82fe66e9f973a11a310e381d2e079bbd847d633a
RodrigoLages <rodrigo_lagesbarbosa@hotmail.com>
2022-03-07 21:23:16 -0300

    //var prompt = function(texto) { return lines.shift();};

---
+++ 1828.js
@@ -0,0 +1,50 @@
+//var prompt = function(texto) { return lines.shift();};
+
+var games = parseInt(prompt());
+const sheldon = [
+  "tesoura papel",
+  "papel pedra",
+  "pedra lagarto",
+  "lagarto Spock",
+  "Spock tesoura",
+  "tesoura lagarto",
+  "lagarto papel",
+  "papel Spock",
+  "Spock pedra",
+  "pedra tesoura",
+];
+
+const raj = [
+  "papel tesoura",
+  "pedra papel",
+  "lagarto pedra",
+  "Spock lagarto",
+  "tesoura Spock",
+  "lagarto tesoura",
+  "papel lagarto",
+  "Spock papel",
+  "pedra Spock",
+  "tesoura pedra",
+];
+
+for (let i = 1; i <= games; i++) {
+  var result = prompt();
+  var draw = true;
+
+  for (let n = 0; n < 10; n++) {
+    if (result == sheldon[n]) {
+      console.log("Caso #" + i + ": Bazinga!");
+      draw = false;
+      break;
+    }
+    if (result == raj[n]) {
+      console.log("Caso #" + i + ": Raj trapaceou!");
+      draw = false;
+      break;
+    }
+  }
+
+  if (draw) {
+    console.log("Caso #" + i + ": De novo!");
+  }
+}
