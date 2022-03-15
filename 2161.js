8e14fb89d9688bd6316c080960e5f06646bf8641
RodrigoLages <rodrigo_lagesbarbosa@hotmail.com>
2022-03-15 18:31:24 -0300

    Square Root of 10

---
+++ 2161.js
@@ -0,0 +1,12 @@
+//var prompt = function(texto) { return lines.shift();};
+
+const repetitions = parseInt(prompt());
+var SR10 = 3;
+var fraction = 0;
+
+for (let i = 0; i < repetitions; i++) {
+  fraction = 1 / (6 + fraction);
+}
+SR10 = SR10 + fraction;
+
+console.log(SR10.toFixed(10));
