85a82635de31865cc2679654561d6f901b148413
RodrigoLages <rodrigo_lagesbarbosa@hotmail.com>
2022-03-16 21:44:30 -0300

    Fast Fibonacci

---
+++ 2164.js
@@ -0,0 +1,7 @@
+//var prompt = function(texto) { return lines.shift();};
+
+const N = parseInt(prompt());
+var term1 = Math.pow((1 + Math.sqrt(5)) / 2, N);
+var term2 = Math.pow((1 - Math.sqrt(5)) / 2, N);
+var fib = (term1 - term2) / Math.sqrt(5);
+console.log(fib.toFixed(1));
