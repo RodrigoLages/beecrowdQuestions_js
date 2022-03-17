1a412b368cdd48ab3391e184ad5e1da4f8ae5444
RodrigoLages <rodrigo_lagesbarbosa@hotmail.com>
2022-03-17 17:24:32 -0300

    Twilight at Portland

---
+++ 2168.js
@@ -0,0 +1,27 @@
+//var prompt = function(texto) { return lines.shift();};
+
+const size = parseInt(prompt()) + 1;
+var blocks = [];
+
+for (let i = 0; i < size; i++) {
+  let aux = prompt().split(" ").map(Number);
+  blocks.push(aux);
+}
+
+for (let r = 0; r < size - 1; r++) {
+  let aux = [];
+  for (let c = 1; c < size; c++) {
+    if (
+      blocks[r][c] +
+        blocks[r][c - 1] +
+        blocks[r + 1][c - 1] +
+        blocks[r + 1][c] >
+      1
+    ) {
+      aux.push("S");
+    } else {
+      aux.push("U");
+    }
+  }
+  console.log(aux.join(""));
+}
