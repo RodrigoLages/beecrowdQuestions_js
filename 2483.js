69dcd55fb9f119c82abad7b20030ea54952e2407
RodrigoLages <rodrigo_lagesbarbosa@hotmail.com>
2022-03-19 16:54:41 -0300

    Merry Christmaaas!

---
+++ 2483.js
@@ -0,0 +1,9 @@
+//var prompt = function(texto) { return lines.shift();};
+
+const N = parseInt(prompt());
+var A = "";
+
+for (let i = 0; i < N; i++) {
+  A = A + "a";
+}
+console.log("Feliz nat" + A + "l!");
