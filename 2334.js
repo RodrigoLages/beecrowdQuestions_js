c86d855714da37836c4a712dd3bbd941f4721505
RodrigoLages <rodrigo_lagesbarbosa@hotmail.com>
2022-03-19 13:59:20 -0300

    Little Ducks

---
+++ 2334.js
@@ -0,0 +1,27 @@
+//var prompt = function(texto) { return lines.shift();};
+
+while (true) {
+  var ducks = prompt();
+  if (ducks < 0) {
+    break;
+  }
+
+  if (0 < ducks && ducks < 10000000000) {
+    ducks = (parseInt(ducks) - 1).toString();
+  } else if (0 < ducks) {
+    var half1 = ducks.slice(-10);
+    var half2 = ducks.slice(0, -10);
+
+    if (half1 > 0) {
+      half1 = (parseInt("1" + half1) - 1).toString();
+      half1 = half1.slice(-10);
+    } else {
+      half1 = "9999999999";
+      half2 = (parseInt(half2) - 1).toString();
+    }
+
+    ducks = half2 + half1;
+  }
+
+  console.log(ducks);
+}
