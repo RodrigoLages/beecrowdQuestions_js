b563af3c70755bb11b79e679c8cac2c03c08e073
RodrigoLages <rodrigo_lagesbarbosa@hotmail.com>
2022-03-23 17:20:10 -0300

    JB6 Team

---
+++ 2626.js
@@ -0,0 +1,34 @@
+//var prompt = function(texto) { return lines.shift();};
+
+const dodo = [
+  "papel pedra pedra",
+  "pedra tesoura tesoura",
+  "tesoura papel papel",
+];
+const leo = [
+  "pedra papel pedra",
+  "tesoura pedra tesoura",
+  "papel tesoura papel",
+];
+const pepper = [
+  "pedra pedra papel",
+  "tesoura tesoura pedra",
+  "papel papel tesoura",
+];
+
+while (true) {
+  var game = prompt();
+  if (game == "") {
+    break;
+  }
+
+  if (dodo.includes(game)) {
+    console.log("Os atributos dos monstros vao ser inteligencia, sabedoria...");
+  } else if (leo.includes(game)) {
+    console.log("Iron Maiden's gonna get you, no matter how far!");
+  } else if (pepper.includes(game)) {
+    console.log("Urano perdeu algo muito precioso...");
+  } else {
+    console.log("Putz vei, o Leo ta demorando muito pra jogar...");
+  }
+}
