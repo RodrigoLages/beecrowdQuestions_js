d695772a0b9ee5921ab24d8d1052956c984be656
RodrigoLages <rodrigo_lagesbarbosa@hotmail.com>
2022-03-24 20:11:08 -0300

    Máquina de Café

---
+++ 2670.js
@@ -0,0 +1,14 @@
+//var prompt = function(texto) { return lines.shift();};
+
+var floors = [];
+floors.push(parseInt(prompt()));
+floors.push(parseInt(prompt()));
+floors.push(parseInt(prompt()));
+
+var minutesEachFloor = [
+  floors[1] * 2 + floors[2] * 4,
+  floors[0] * 2 + floors[2] * 2,
+  floors[0] * 4 + floors[1] * 2,
+];
+
+console.log(Math.min(...minutesEachFloor));
