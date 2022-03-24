894f294ecc528bef4c36cb07716293f75ad297cc
RodrigoLages <rodrigo_lagesbarbosa@hotmail.com>
2022-03-24 14:50:10 -0300

    Dijkstra

---
+++ 2653.js
@@ -0,0 +1,16 @@
+//var prompt = function(texto) { return lines.shift();};
+
+var treasure = [];
+
+while (true) {
+  var jewel = prompt();
+  if (jewel == "") {
+    break;
+  }
+
+  if (treasure.includes(jewel) == false) {
+    treasure.push(jewel);
+  }
+}
+
+console.log(treasure.length);
