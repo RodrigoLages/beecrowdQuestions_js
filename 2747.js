a898c4943f8cc719044607c8c871bc7b4981ac66
RodrigoLages <rodrigo_lagesbarbosa@hotmail.com>
2022-03-31 18:08:00 -0300

    Output 1

---
+++ 2747.js
@@ -0,0 +1,19 @@
+var width = "";
+var height = "";
+
+for (let i = 0; i < 39; i++) {
+  width = width + "-";
+  if (i == 0 || i == 38) {
+    height = height + "|";
+  } else {
+    height = height + " ";
+  }
+}
+
+console.log(width);
+console.log(height);
+console.log(height);
+console.log(height);
+console.log(height);
+console.log(height);
+console.log(width);
