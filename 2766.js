19a76975aaa46090f5c243105b97a7d9408d3a12
RodrigoLages <rodrigo_lagesbarbosa@hotmail.com>
2022-04-05 20:03:39 -0300

    Input and Output Reading and Skipping Names

---
+++ 2766.js
@@ -0,0 +1,9 @@
+//var prompt = function(texto) { return lines.shift();};
+
+for (let i = 0; i < 10; i++) {
+  var nam = prompt();
+
+  if (i == 2 || i == 6 || i == 8) {
+    console.log(nam);
+  }
+}
