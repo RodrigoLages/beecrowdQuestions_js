cd2fba8eaeee227ffd7e4efaca1a2333da855c27
RodrigoLages <rodrigo_lagesbarbosa@hotmail.com>
2022-04-09 22:00:15 -0300

    Sensor Accuracy

---
+++ 2774.js
@@ -0,0 +1,25 @@
+//var prompt = function(texto) { return lines.shift();};
+
+while (true) {
+  var inpt = prompt();
+  if (inpt == "") {
+    break;
+  }
+
+  var measures = prompt().split(" ").map(Number);
+
+  var mean = 0;
+  for (let i = 0; i < measures.length; i++) {
+    mean += measures[i];
+  }
+  mean = mean / measures.length;
+
+  var summation = 0;
+  for (let i = 0; i < measures.length; i++) {
+    summation += (measures[i] - mean) * (measures[i] - mean);
+  }
+
+  var accuracy = Math.sqrt(summation / (measures.length - 1));
+
+  console.log(accuracy.toFixed(5));
+}
