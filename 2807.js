269e7190847f1e4d646283f7a220f9a4d89eb971
RodrigoLages <rodrigo_lagesbarbosa@hotmail.com>
2022-04-11 12:53:07 -0300

    Iccanobif

---
+++ 2807.js
@@ -0,0 +1,12 @@
+//var prompt = function(texto) { return lines.shift();};
+
+const size = parseInt(prompt());
+var fib = [1, 1];
+
+for (let i = 1; i < size; i++) {
+  fib.push(fib[i] + fib[i - 1]);
+}
+
+fib.length = size;
+
+console.log(fib.reverse().join(" "));
