34dd07348424db004dc51d9239a7208eb547383b
RodrigoLages <rodrigo_lagesbarbosa@hotmail.com>
2022-04-12 14:45:52 -0300

    Avoiding Rain

---
+++ 2813.js
@@ -0,0 +1,34 @@
+//var prompt = function(texto) { return lines.shift();};
+
+const cases = parseInt(prompt());
+
+var weatherHome;
+var weatherWork;
+var storeHome = 0;
+var storeWork = 0;
+var home = 0;
+var work = 0;
+
+for (let i = 0; i < cases; i++) {
+  [weatherHome, weatherWork] = prompt().split(" ");
+
+  if (weatherHome == "chuva") {
+    if (home > 0) {
+      home--;
+    } else {
+      storeHome++;
+    }
+    work++;
+  }
+
+  if (weatherWork == "chuva") {
+    if (work > 0) {
+      work--;
+    } else {
+      storeWork++;
+    }
+    home++;
+  }
+}
+
+console.log(storeHome + " " + storeWork);
