72611e3437fa7a33abbfe98d325befab2d621027
RodrigoLages <rodrigo_lagesbarbosa@hotmail.com>
2022-04-13 12:41:36 -0300

    Party at the North Pole

---
+++ 2845.js
@@ -0,0 +1,31 @@
+//var prompt = function(texto) { return lines.shift();};
+
+function areCoprimes(min, max) {
+  for (let i = 2; i <= min; i++) {
+    if (min % i == 0 && max % i == 0) {
+      return false;
+    }
+  }
+  return true;
+}
+
+function areAllCoprimes(arr, num) {
+  for (let item of arr) {
+    if (!areCoprimes(item, num)) {
+      return false;
+    }
+  }
+  return true;
+}
+
+prompt();
+const identifiers = prompt().split(" ").map(Number);
+
+var highest = Math.max(...identifiers);
+for (var id = highest + 1; true; id++) {
+  if (areAllCoprimes(identifiers, id)) {
+    break;
+  }
+}
+
+console.log(id);
