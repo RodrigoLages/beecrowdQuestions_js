6754c92e7a7a88d6d411c77a993c7d9d0eb6f005
RodrigoLages <rodrigo_lagesbarbosa@hotmail.com>
2022-04-17 17:33:50 -0300

    How Much Cassava?

---
+++ 2936.js
@@ -0,0 +1,11 @@
+//var prompt = function(texto) { return lines.shift();};
+
+var cassava = 225;
+
+cassava += parseInt(prompt()) * 300;
+cassava += parseInt(prompt()) * 1500;
+cassava += parseInt(prompt()) * 600;
+cassava += parseInt(prompt()) * 1000;
+cassava += parseInt(prompt()) * 150;
+
+console.log(cassava);
