d593861e310b4e6d0d29df5d526ace54b5d32ffd
RodrigoLages <rodrigo_lagesbarbosa@hotmail.com>
2022-04-17 14:12:29 -0300

    Digits

---
+++ 2867.js
@@ -0,0 +1,11 @@
+//var prompt = function(texto) { return lines.shift();};
+
+const cases = parseInt(prompt());
+
+for (let i = 0; i < cases; i++) {
+  var [base, exp] = prompt().split(" ").map(Number);
+
+  var digits = Math.trunc(Math.log10(base) * exp);
+
+  console.log(digits + 1);
+}
