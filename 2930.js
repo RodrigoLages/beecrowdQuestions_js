3685f629120ecd3601dcf945cd6a216fd69fceb5
RodrigoLages <rodrigo_lagesbarbosa@hotmail.com>
2022-04-17 16:37:37 -0300

    Final Thesis of Christmas Depression

---
+++ 2930.js
@@ -0,0 +1,17 @@
+//var prompt = function(texto) { return lines.shift();};
+
+var [deliver, due] = prompt().split(" ").map(Number);
+
+if (deliver > due) {
+  console.log("Eu odeio a professora!");
+} else if (due - deliver >= 3) {
+  console.log("Muito bem! Apresenta antes do Natal!");
+} else {
+  console.log("Parece o trabalho do meu filho!");
+
+  if (deliver + 2 < 24) {
+    console.log("TCC Apresentado!");
+  } else {
+    console.log("Fail! Entao eh nataaaaal!");
+  }
+}
