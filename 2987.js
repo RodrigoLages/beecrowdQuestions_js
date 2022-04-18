d39c5245c1d333ab2321c8efa194ad066f077040
RodrigoLages <rodrigo_lagesbarbosa@hotmail.com>
2022-04-18 11:59:46 -0300

    Balloon of Honor

---
+++ 2987.js
@@ -0,0 +1,6 @@
+//var prompt = function(texto) { return lines.shift();};
+
+const alphabet = "_ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
+
+var letter = prompt();
+console.log(alphabet.indexOf(letter));
