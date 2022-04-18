f01fc54340055b96eb37bdb88f42255dd94ce945
RodrigoLages <rodrigo_lagesbarbosa@hotmail.com>
2022-04-18 10:57:27 -0300

    The Return of The King

---
+++ 2951.js
@@ -0,0 +1,26 @@
+//var prompt = function(texto) { return lines.shift();};
+
+const [ammount, needed] = prompt().split(" ").map(Number);
+const runes = new Map();
+
+for (let i = 0; i < ammount; i++) {
+  let rune = prompt().split(" ");
+  rune[1] = parseInt(rune[1]);
+  runes.set(rune[0], rune[1]);
+}
+
+prompt();
+
+var recited = prompt().split(" ");
+
+var sum = 0;
+for (let item of recited) {
+  sum += runes.get(item);
+}
+
+console.log(sum);
+if (sum >= needed) {
+  console.log("You shall pass!");
+} else {
+  console.log("My precioooous");
+}
